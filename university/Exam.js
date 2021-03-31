1dbec20395be4232d9e4b81261e455384022715e
Md.Shafikul Islam <63946398+Shafikul-islam1@users.noreply.github.com>
2021-03-31 18:09:25 +0600

    Add files via upload

---
+++ university/Exam.js
@@ -0,0 +1,43 @@
+const _id = Symbol('id');
+const _name = Symbol('name');
+const _passMark = Symbol('passMark');
+const _subject = Symbol('subject');
+class Exam{
+    constructor(id, name,subject,passMark){
+        this[_id] = id; 
+        this[_name] = name;
+        this[_subject] = subject;
+        this[_passMark] = passMark || 33;
+    }
+    get id(){
+        return this[_id];
+    }
+    get name(){
+        return this[_name];
+    }
+    set name(value) {
+        this[_name] = value;
+    }
+    get passMark(){
+        return this[_passMark];
+    }
+    set passMark(value) {
+        this[_passMark] = value;
+    }
+    get subject(){
+        return this[_subject];
+    }
+    set subject(value) {
+        this[_subject] = value;
+    }
+    
+    toString() {
+        return `
+         ID :${this[_id]},
+         Nmae :${this[_name]},
+         Subject :${this[_subject]},
+         passMark :${this[_passMark]}
+         `;
+    }
+}
+module.exports =Exam;
